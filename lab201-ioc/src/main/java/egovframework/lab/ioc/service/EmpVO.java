a780f37eb56e92af7c04617e720fab36076334c9
dylee <egni1227@naver.com>
2025-05-14 11:48:18 +0900

    [lab201-ioc] : XML 설정 방식의 Spring bean 서비스 작성
    - EmpService.java: EmpService 에 대한 CRUD 및 목록조회 비지니스 메서드 작성
    - EmpVO.java: EmpVO 작성
    - XmlEmpServiceImpl.java: XmlEmpServiceImpl 작성
    - XmlEmpDAO.java: XmlEmpDAO 작성
    - context-emp.xml: xmlEmpService, xmlEmpDAO 에 대한 Spring bean 설정 작성

--- lab201-ioc/src/main/java/egovframework/lab/ioc/service/EmpVO.java
+++ lab201-ioc/src/main/java/egovframework/lab/ioc/service/EmpVO.java
@@ -4,8 +4,51 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
-public class EmpVO implements Serializable {
+public class EmpVO implements Serializable, Comparable<EmpVO> {
 
-    // TODO [Step 1-2, 2-2, 3-1] EmpVO 작성
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 2136423284978691217L;
 
+	// TODO [Step 1-2, 2-2, 3-1] EmpVO 작성
+	private int empNo;
+
+	private String empName;
+
+	private String job;
+
+	public int getEmpNo() {
+		return empNo;
+	}
+
+	public void setEmpNo(int empNo) {
+		this.empNo = empNo;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+	
+	public int compareTo(EmpVO o) {
+		if (empNo > o.getEmpNo()) {
+			return 1;
+		} else if (empNo < o.getEmpNo()) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
 }
